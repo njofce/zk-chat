ca1b122b44e90bd42ade9554aac5e7e408c5699f
Nick Tampakas <nick@gmx.co.uk>
2023-03-22 15:17:42 +0200

    Check num of containers running + check if ports are exposed

--- scripts/validate_app.sh
+++ scripts/validate_app.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker ps | grep docker-entrypoint
-[ $? -eq 0 ] || exit 1
+containers_running=$(docker ps --format "{{.Status}}" | grep -ci up)
+[ $containers_running -eq 4 ] || exit 1
+
+exposed_ports=$(netstat -lnt4 | egrep -cw '3000|8080|8081')
+[ $exposed_ports -eq 3 ] || exit 1
 
 exit 0
