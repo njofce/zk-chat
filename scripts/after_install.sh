e78df9c6a403976721dd582fca9b4f4451127127
Nick Tampakas <nick@gmx.co.uk>
2023-03-22 14:45:36 +0200

    Add build killswitch + minnor changes in scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
  
-echo "Nothing here"
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
  
 exit 0
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
-
 cd ~/zk-chat-client-server
 docker compose -f docker-compose-aws.yaml up -d
 
