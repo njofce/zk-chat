20c0fa56c82e3f0a10fff1cb84a732036b1e099f
Nico Serrano <nnicosp@hotmail.com>
2023-04-25 01:22:05 -0500

    server(fix): use semaphore syncer

--- zk-chat-server-lib/src/create_server.ts
+++ zk-chat-server-lib/src/create_server.ts
@@ -11,7 +11,6 @@ import SocketServer from "./communication/socket/socket_server";
 import MessageHandlerService from "./services/message_handler_service";
 import NodeSynchronizer from "./communication/node_sync";
 import SemaphoreSynchronizer from "./semaphore";
-import InterRepSynchronizer from "./interrep";
 import Hasher from "./util/hasher";
 import UserService from "./services/user.service";
 import KeyExchangeService from "./services/key_exchange_service";
@@ -49,8 +48,8 @@ const initZKChatServer = async (config: IZKServerConfig) => {
 
     const keyExchangeService: KeyExchangeService = createKeyExchangeService(config, userService, keyExchangeRequestStatsService, new Hasher());
 
-    const interRepSynchronizer = new InterRepSynchronizer(redisPubSub, groupService, userService, config);
-    await interRepSynchronizer.sync();
+    const semaphoreSynchronizer = new SemaphoreSynchronizer(redisPubSub, groupService, userService, config);
+    await semaphoreSynchronizer.sync();
 
     const messageHandler: MessageHandlerService = createMessageHandler(config, redisPubSub, userService, requestStatsService);
     const socketServer: SocketServer = createSocketServer(config, messageHandler.handleChatMessage);
