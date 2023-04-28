8f3f451cf37411e913931bdfe4fa051674dfafe9
mhchia <kevin.mh.chia@gmail.com>
2023-04-28 13:16:21 +0800

    fix: json-bigint is not present in package.json

--- app/src/constants/zuzalu.ts
+++ app/src/constants/zuzalu.ts
@@ -1,13 +1,4 @@
 export const PCD_GITHUB_URL = "https://github.com/proofcarryingdata/pcd";
 
-export const IS_PROD = process.env.NODE_ENV === "production";
-console.log(`!@# IS_PROD = `, IS_PROD)
-
-// export const PASSPORT_URL = IS_PROD
-//   // ? "https://zupass.org/"
-//   ? "http://13.112.238.119:3000/"
-//   : "http://localhost:3000/";
-
-
+// export const PASSPORT_URL = "https://zupass.org";
 export const PASSPORT_URL = "http://13.112.238.119:3000";
-// export const PASSPORT_URL = "http://localhost:3000";
