895295da29d31d3e25fb8e24dfc97aa60c7f4553
mhchia <kevin.mh.chia@gmail.com>
2023-04-29 17:31:58 +0800

    change url to zuzalu passport

--- app/src/constants/zuzalu.ts
+++ app/src/constants/zuzalu.ts
@@ -1,4 +1,4 @@
 export const PCD_GITHUB_URL = "https://github.com/proofcarryingdata/pcd";
 
-// export const PASSPORT_URL = "https://zupass.org";
-export const PASSPORT_URL = "http://13.112.238.119:3000";
+export const PASSPORT_URL = "https://zupass.org";
+// export const PASSPORT_URL = "http://13.112.238.119:3000";
